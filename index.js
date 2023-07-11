628ae1acffedb4f48a42437280d957c37db5de96
José Miguel Carrera <jm.carrera@lapieza.io>
2023-07-11 17:55:19 -0600

    get users

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 8000;
 const {db} = require("./src/services/firebase/database")
+const {firebaseAuth} = require("./src/services/firebase/auth")
 const routes = require("./src/routes")
 
 app.use(db);
+app.use(firebaseAuth);
+app.use(express.json());
 
 routes(app);
 
---
+++ src/components/users/network.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const Controller = require("./controller")
+
+const router = express.Router();
+
+function addUser(req, res) {
+    const { auth } = req;
+    const {
+        email,
+        password,
+        displayName
+    } = req.body;
+
+    Controller.addUser({
+        auth,
+        email,
+        password,
+        displayName
+    })
+    .then((result) => res.send(result))
+    .catch((error) => res.send(error))
+
+}
+
+function getUser(req, res) {
+    const { auth } = req;
+    const { email } = req.body;
+
+    Controller.getUser({
+        auth,
+        email
+    })
+    .then((result) => res.send(result))
+    .catch((error) => res.send(error))
+}
+
+// /users post agregar
+// /users patch actualizar
+// /users delete eliminar
+// /users get obtener
+
+router.post("/", addUser)
+router.get("/get-user", getUser)
+router.patch("/", () => {})
+
+module.exports = router
